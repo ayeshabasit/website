78ca2b8abcd30f1a9ae36a72e1d3edfd9518228f
Ayesha <ayeshabasitalvi@gmail.com>
2024-08-28 20:39:59 -0400

    fixed mobile responsiveness issues

--- components/testimonial-card.tsx
+++ components/testimonial-card.tsx
@@ -7,7 +7,8 @@ type TestimonialCardProps = {
   name: string;
   company: string;
   quote: string;
-  link: string;
+  linkedIn: string;
+  companyLink: string;
 };
 
 export default function TestimonialCard({
@@ -15,7 +16,8 @@ export default function TestimonialCard({
   name,
   company,
   quote,
-  link,
+  companyLink,
+  linkedIn,
 }: TestimonialCardProps) {
   return (
     <div
@@ -53,11 +55,25 @@ export default function TestimonialCard({
       <blockquote className="text-lg text-gray-400 mt-4 grow">
         {quote}
       </blockquote>
-      <div className="text-gray-700 font-medium mt-6 pt-5 border-t border-gray-700">
-        <cite className="text-gray-200 not-italic">{name}</cite> -{" "}
+      <div className="text-gray-700 font-medium mt-6 pt-5 border-t border-gray-700 flex justify-between items-center">
+        <a
+          href={linkedIn}
+          className="text-gray-200 not-italic flex items-center"
+        >
+          <svg
+            className="w-8 h-8 fill-current"
+            viewBox="0 0 32 32"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path d="M23.3 8H8.7c-.4 0-.7.3-.7.7v14.7c0 .3.3.6.7.6h14.7c.4 0 .7-.3.7-.7V8.7c-.1-.4-.4-.7-.8-.7zM12.7 21.6h-2.3V14h2.4v7.6h-.1zM11.6 13c-.8 0-1.4-.7-1.4-1.4 0-.8.6-1.4 1.4-1.4.8 0 1.4.6 1.4 1.4-.1.7-.7 1.4-1.4 1.4zm10 8.6h-2.4v-3.7c0-.9 0-2-1.2-2s-1.4 1-1.4 2v3.8h-2.4V14h2.3v1c.3-.6 1.1-1.2 2.2-1.2 2.4 0 2.8 1.6 2.8 3.6v4.2h.1z" />
+          </svg>
+
+          {name}
+        </a>{" "}
+        -{" "}
         <a
           className="text-athenix-bright-green hover:text-gray-200 transition duration-150 ease-in-out"
-          href={link}
+          href={companyLink}
         >
           {company}
         </a>
